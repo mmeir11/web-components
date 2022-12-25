5d96a3fa4869ca8a1ac28b41474e768f00b04819
meira <meira@cymulate.com>
2022-12-25 17:41:51 +0200

    date

--- pages/api/health.js
+++ pages/api/health.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ health: true })
+  res.status(200).json({ health: true, date: new Date() })
 }
